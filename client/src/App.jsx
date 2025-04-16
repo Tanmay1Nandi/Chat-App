479db36b06cb4fb2517ac27a8f57bb258be60526
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-04-16 11:43:06 +0530

    Installed react and shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="bg-gradient-to-br from-purple-500 to-blue-500 cursor-pointer">Click me</Button>
+    </div>
+  )
+}
+
+export default App
+
