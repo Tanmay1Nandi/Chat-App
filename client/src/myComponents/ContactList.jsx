2aa692dc800801be8fdba706e579ecc18308562e
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-05-03 06:58:05 +0530

    .

--- client/src/myComponents/ContactList.jsx
+++ client/src/myComponents/ContactList.jsx
@@ -17,7 +17,13 @@ export default function ContactList({contacts, isChannel = false}) {
   return (
     <div className='mt-5'>
       {contacts.map((contact) => (
-        <div key={contact._id} className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${selectedChatData && selectedChatData._id === contact._id? "bg-[#8417ff]} hover:bg-[#8417ff]" : "hover:bg-[#ff11ff]/50"}`} onClick={() => handleClick(contact)}>
+        <div  key={contact._id}
+        className={`pl-10 py-2 transition-all duration-300 cursor-pointer ${
+          selectedChatData && selectedChatData._id === contact._id
+            ? 'bg-[#8417ff] hover:bg-[#8417ff]'
+            : 'hover:bg-[#ff11ff]/50'
+        }`}
+        onClick={() => handleClick(contact)}>
             <div className="flex gap-5 items-center justify-start text-neutral-300">
                 {
                     !isChannel && <div className="">
