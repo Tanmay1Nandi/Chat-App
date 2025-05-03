fa5b75d40ce3a2f98e584cf086252e6a8ffc23ed
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-05-03 06:38:04 +0530

    .

--- api/index.js
+++ api/index.js
@@ -47,7 +47,7 @@ app.use("/api/channels", channelRouter);
 if (NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "..", "client", "dist")));
   
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
       res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
     });
   }
