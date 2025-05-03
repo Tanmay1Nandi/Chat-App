0b3f0b5b8d05db128d54d37c1f0c002524b53894
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-05-03 11:53:46 +0530

    ui

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -336,7 +336,7 @@ export default function MessageContainer() {
 
 
   return (
-    <div className='flex-1 overflow-y-auto overflow-x-hidden scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full'>
+    <div className='flex-1 overflow-y-auto overflow-x-hidden scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full pb-0 sm:pb-4'>
       {renderMessages()}
       <div ref={scrollRef} className=""></div>
     </div>
